3b7cfd183c70f6b30eb263d6d6b5a4eb0fd4dff8
Kirill Shkrogalev <kirill.shkrogalev@gmail.com>
2026-01-27 08:27:50 +0300

    Added check HTTP method

--- internal/handler/shortener/service.go
+++ internal/handler/shortener/service.go
@@ -6,6 +6,10 @@ import (
 )
 
 func UrlEncode(resp http.ResponseWriter, req *http.Request) {
+	if req.Method != http.MethodPost {
+		resp.WriteHeader(http.StatusBadRequest)
+		return
+	}
 	resp.Header().Set("Content-Type", "text/plain")
 	resp.Header().Add("Content-Length", "30")
 	resp.WriteHeader(http.StatusCreated)
@@ -15,4 +19,13 @@ func UrlEncode(resp http.ResponseWriter, req *http.Request) {
 	if err != nil {
 		log.Printf("don't send response because by %s\n", err.Error())
 	}
+}
+
+func UrlDecode(resp http.ResponseWriter, req *http.Request) {
+	if req.Method != http.MethodGet {
+		resp.WriteHeader(http.StatusBadRequest)
+		return
+	}
+	resp.Header().Set("Location", "https://practicum.yandex.ru")
+	resp.WriteHeader(http.StatusTemporaryRedirect)
 }
