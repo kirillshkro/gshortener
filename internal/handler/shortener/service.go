c5727427104a22fc0c02ccddf8d412c5c570c6c8
Kirill Shkrogalev <kirill.shkrogalev@gmail.com>
2026-01-27 07:40:56 +0300

    Added UrlEncode handler

---
+++ internal/handler/shortener/service.go
@@ -0,0 +1,18 @@
+package shortener
+
+import (
+	"log"
+	"net/http"
+)
+
+func UrlEncode(resp http.ResponseWriter, req *http.Request) {
+	resp.Header().Set("Content-Type", "text/plain")
+	resp.Header().Add("Content-Length", "30")
+	resp.WriteHeader(http.StatusCreated)
+
+	body := []byte("http://localhost:8080/EwHXdJfB")
+	_, err := resp.Write(body)
+	if err != nil {
+		log.Printf("don't send response because by %s\n", err.Error())
+	}
+}
