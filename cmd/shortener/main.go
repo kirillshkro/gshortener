2dbef5c7c3f13943e19f69bfff384f962471d92e
Kirill Shkrogalev <kirill.shkrogalev@gmail.com>
2026-02-01 14:31:13 +0300

    Added function parseFlags

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -1,19 +1,31 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"net/http"
 
 	"github.com/gorilla/mux"
+	"github.com/kirillshkro/gshortener/internal/config"
 	"github.com/kirillshkro/gshortener/internal/handler/shortener"
 )
 
+var cfg *config.Config
+
 func main() {
+	parseFlags()
 	mux := mux.NewRouter()
 	//Добавляем хандлеры
 	mux.HandleFunc("/", shortener.URLEncode)
 	mux.HandleFunc("/{id}", shortener.URLDecode).Methods(http.MethodGet)
-	if err := http.ListenAndServe(":8080", mux); err != nil {
+	if err := http.ListenAndServe(cfg.Address, mux); err != nil {
 		fmt.Printf("error listen server is %s\n", err.Error())
 	}
+}
+
+func parseFlags() {
+	cfg = config.NewConfig()
+	flag.StringVar(&cfg.Address, "a", cfg.Address, "Set base host address service")
+	flag.StringVar(&cfg.ShortedURL, "b", cfg.ShortedURL, "Set base shorted url")
+	flag.Parse()
 }
