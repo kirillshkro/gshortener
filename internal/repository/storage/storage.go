7867fe4e031e3ab5166a4271e3d9f918b4053c63
Kirill Shkrogalev <kirill.shkrogalev@gmail.com>
2026-02-08 11:36:33 +0300

    Added mutex for sync

--- internal/repository/storage/storage.go
+++ internal/repository/storage/storage.go
@@ -1,9 +1,14 @@
 package storage
 
-import "github.com/kirillshkro/gshortener/internal/types"
+import (
+	"sync"
+
+	"github.com/kirillshkro/gshortener/internal/types"
+)
 
 type Storage struct {
 	data map[types.ShortURL]types.RawURL
+	m    sync.Mutex
 }
 
 type IStorage interface {
@@ -17,14 +22,16 @@ func NewStorage() *Storage {
 	}
 }
 
-func (s Storage) Data(key types.ShortURL) types.RawURL {
+func (s *Storage) Data(key types.ShortURL) types.RawURL {
 	return s.data[key]
 }
 
-func (s Storage) SetData(key types.ShortURL, val types.RawURL) {
+func (s *Storage) SetData(key types.ShortURL, val types.RawURL) {
+	s.m.Lock()
 	if key != "" && val != "" {
 		if _, exist := s.data[key]; !exist {
 			s.data[key] = val
 		}
 	}
+	s.m.Unlock()
 }
