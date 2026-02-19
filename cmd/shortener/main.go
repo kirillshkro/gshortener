1c102facdab606081ad7500d6676ffb609b31749
Kirill Shkrogalev <kirill.shkrogalev@gmail.com>
2026-02-19 23:25:23 +0300

    Added compress handlers

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -18,9 +18,13 @@ func main() {
 	service := shortener.NewServiceWithAddrWithAddrShortener(types.RawURL(cfg.Address), types.ShortURL(cfg.ShortedURL))
 	mux := mux.NewRouter()
 	//Добавляем хандлеры с логгированием
-	mux.Handle("/", shortener.HandlerWithLog(shortener.EncodeHandler(service))).Methods(http.MethodPost)
-	mux.Handle("/{id}", shortener.HandlerWithLog(shortener.DecodeHandler(service))).Methods(http.MethodGet)
-	mux.Handle("/api/shorten", shortener.HandlerWithLog(shortener.CreateShortURLHandler(service))).Methods(http.MethodPost)
+	mux.Handle("/", shortener.HandlerWithCompress(
+		shortener.HandlerWithLog(
+			shortener.EncodeHandler(service)))).Methods(http.MethodPost)
+	mux.Handle("/{id}", shortener.HandlerWithCompress(
+		shortener.HandlerWithLog(shortener.DecodeHandler(service)))).Methods(http.MethodGet)
+	mux.Handle("/api/shorten", shortener.HandlerWithCompress(
+		shortener.HandlerWithLog(shortener.CreateShortURLHandler(service)))).Methods(http.MethodPost)
 	if err := http.ListenAndServe(cfg.Address, mux); err != nil {
 		fmt.Printf("error listen server is %s\n", err.Error())
 	}
