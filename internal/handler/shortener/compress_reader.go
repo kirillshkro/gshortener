b342351d4efdbdf6e02377c9ff6b3af4ae061748
Kirill Shkrogalev <kirill.shkrogalev@gmail.com>
2026-02-23 12:19:37 +0300

    Added constructor to custom reader

---
+++ internal/handler/shortener/compress_reader.go
@@ -0,0 +1,40 @@
+package shortener
+
+import (
+	"compress/gzip"
+	"compress/zlib"
+	"errors"
+	"io"
+)
+
+type compReader struct {
+	r        io.ReadCloser
+	compType string
+	zr       *io.ReadCloser
+}
+
+func newCompReader(r io.ReadCloser, compType string) (*compReader, error) {
+	var (
+		zr  io.ReadCloser
+		err error
+	)
+	switch compType {
+	case "gzip":
+		zr, err = gzip.NewReader(r)
+		if err != nil {
+			return nil, err
+		}
+	case "deflate":
+		zr, err = zlib.NewReader(r)
+		if err != nil {
+			return nil, err
+		}
+	default:
+		return nil, errors.New("Unknown compression format")
+	}
+
+	return &compReader{
+		r:  r,
+		zr: &zr,
+	}, nil
+}
