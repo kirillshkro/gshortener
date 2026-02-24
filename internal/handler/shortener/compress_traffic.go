c32904c084998a6a4143b5a47da2a44111b94e2c
Kirill Shkrogalev <kirill.shkrogalev@gmail.com>
2026-02-24 14:24:17 +0300

    Refactoring

--- internal/handler/shortener/compress_traffic.go
+++ internal/handler/shortener/compress_traffic.go
@@ -15,9 +15,11 @@ type respCompressWriter struct {
 func HandlerWithCompress(next http.Handler) http.Handler {
 	compressFn := func(w http.ResponseWriter, r *http.Request) {
 		if isCompContent(r) {
+			var rBody io.ReadCloser
 			compEncoding := r.Header.Get("Accept-Encoding")
 			if compEncoding == "gzip" || compEncoding == "deflate" {
-				compReader, err := newCompReader(r.Body, compEncoding)
+				rBody = r.Body
+				compReader, err := newCompReader(rBody, compEncoding)
 				if err != nil {
 					http.Error(w, "Internal error", http.StatusBadRequest)
 					return
