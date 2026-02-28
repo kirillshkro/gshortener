6353282fbf1829d239d242e843a4f14c74404721
Kirill Shkrogalev <kirill.shkrogalev@gmail.com>
2026-02-28 13:14:38 +0300

    Updated config info

--- internal/config/config.go
+++ internal/config/config.go
@@ -9,24 +9,32 @@ import (
 type Config struct {
 	Address    string `json:"address"`
 	ShortedURL string `json:"shorted_url"`
+	FileDb     string `json:"file_db"`
 }
 
 func NewConfig() *Config {
 	var (
-		baseAddress string
-		shorted     string
-		exists      bool
+		baseAddress     string
+		shorted         string
+		fileStoragePath string
+		exists          bool
 	)
 
 	if baseAddress, exists = os.LookupEnv("SERVER_ADDRESS"); !exists {
 		baseAddress = "localhost:8080"
 	}
+
 	if shorted, exists = os.LookupEnv("BASE_URL"); !exists {
 		shorted = "http://localhost:8080"
+	}
+
+	if fileStoragePath, exists = os.LookupEnv("FILE_STORAGE_PATH"); !exists {
+		fileStoragePath = "/var/run/shortener.json"
 	}
 
 	return &Config{
 		Address:    baseAddress,
 		ShortedURL: shorted,
+		FileDb:     fileStoragePath,
 	}
 }
