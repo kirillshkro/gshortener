9a0d6df6b7a7b4bca254bd070b187db0abaab6f6
Kirill Shkrogalev <kirill.shkrogalev@gmail.com>
2026-03-02 14:02:39 +0300

    Added tests

---
+++ internal/handler/shortener/middleware_test.go
@@ -0,0 +1,62 @@
+package shortener
+
+import (
+	"bytes"
+	"encoding/json"
+	"net/http"
+	"net/http/httptest"
+	"strings"
+	"testing"
+	"time"
+
+	"github.com/stretchr/testify/assert"
+)
+
+type logCaptureWriter struct {
+	strings.Builder
+}
+
+func (l *logCaptureWriter) Write(p []byte) (n int, err error) {
+	return l.Builder.Write(p)
+}
+
+func TestHandlerWithLog(t *testing.T) {
+	service := NewService()
+
+	wrapped := HandlerWithLog(DecodeHandler(service))
+
+	req := httptest.NewRequest("GET", "/test", nil)
+	w := httptest.NewRecorder()
+
+	start := time.Now()
+	wrapped.ServeHTTP(w, req)
+	elapsed := time.Since(start)
+	resp := w.Result()
+	defer resp.Body.Close()
+
+	assert.Equal(t, http.StatusTemporaryRedirect, resp.StatusCode)
+	assert.NotZero(t, elapsed)
+
+	reqData := RequestData{
+		URL: "https://weather.google.com",
+	}
+
+	body := bytes.NewBuffer(nil)
+	if err := json.NewEncoder(body).Encode(reqData); err != nil {
+		t.Fatal(err)
+	}
+	req = httptest.NewRequest(http.MethodPost, "/api/shorten", body)
+	w = httptest.NewRecorder()
+
+	start = time.Now()
+
+	wrapped = HandlerWithLog(EncodeHandler(service))
+	wrapped.ServeHTTP(w, req)
+	elapsed = time.Since(start)
+	resp = w.Result()
+	defer resp.Body.Close()
+
+	if assert.Equal(t, http.StatusCreated, resp.StatusCode) {
+		assert.Greater(t, elapsed, time.Millisecond*0)
+	}
+}
