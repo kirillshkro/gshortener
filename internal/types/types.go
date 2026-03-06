1ec4f78121a85d32fddc115687762dac6236c0d6
Kirill Shkrogalev <kirill.shkrogalev@gmail.com>
2026-03-06 19:59:49 +0300

    Type field UUID changed to string

--- internal/types/types.go
+++ internal/types/types.go
@@ -6,7 +6,7 @@ type RawURL string
 type ShortURL string
 
 type FileData struct {
-	UUID        int64    `json:"uuid"`
+	UUID        string   `json:"uuid"`
 	ShortURL    ShortURL `json:"short_url"`
 	OriginalURL RawURL   `json:"original_url"`
 }
