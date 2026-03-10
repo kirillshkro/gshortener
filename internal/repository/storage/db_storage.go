308489a35310de1b6c1bda963d9a5e69c057e584
Kirill Shkrogalev <kirill.shkrogalev@gmail.com>
2026-03-10 12:19:10 +0300

    Removed sql-driven code

--- internal/repository/storage/db_storage.go
+++ internal/repository/storage/db_storage.go
@@ -30,25 +30,6 @@ func newDBStorage(connString string) (*DBStorage, error) {
 	if err != nil {
 		return nil, err
 	}
-	//Создать базу данных
-	if _, err = db.Exec(`create database if not exists shortener`); err != nil {
-		return nil, err
-	}
-	//добавляем таблицу urls
-	if _, err = db.Exec(`create table if not exists urls (id integer generated always as identity primary key, 
-	short_url varchar(255) not null, 
-	original_url text not null)`); err != nil {
-		return nil, err
-	}
-	//добавляем индекс для поля short_url
-	if _, err = db.Exec(`create unique index if not exists idx_short_url on urls (short_url)`); err != nil {
-		return nil, err
-	}
-
-	//добавляем ограничение уникальности для поля original_url
-	if _, err = db.Exec(`create index if not exists idx_original_url on urls (original_url)`); err != nil {
-		return nil, err
-	}
 
 	return &DBStorage{db: db}, nil
 }
