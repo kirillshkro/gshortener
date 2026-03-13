dbcdb22e03e836093a9f7eeb1f647498b6b8d99c
Kirill Shkrogalev <kirill.shkrogalev@gmail.com>
2026-03-13 20:48:54 +0300

    Added cleanenv to Config

--- go.mod
+++ go.mod
@@ -10,14 +10,18 @@ require (
 )
 
 require (
+	github.com/BurntSushi/toml v1.2.1 // indirect
+	github.com/joho/godotenv v1.5.1 // indirect
 	github.com/mattn/go-colorable v0.1.13 // indirect
 	github.com/mattn/go-isatty v0.0.19 // indirect
 	golang.org/x/sys v0.12.0 // indirect
+	olympos.io/encoding/edn v0.0.0-20201019073823-d3554ca0b0a3 // indirect
 )
 
 require (
 	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/google/uuid v1.6.0
+	github.com/ilyakaznacheev/cleanenv v1.5.0
 	github.com/pmezard/go-difflib v1.0.0 // indirect
 	github.com/rs/zerolog v1.34.0
 	gopkg.in/yaml.v3 v3.0.1 // indirect
--- internal/config/config.go
+++ internal/config/config.go
@@ -1,8 +1,9 @@
 package config
 
 import (
-	"os"
 	"sync"
+
+	"github.com/ilyakaznacheev/cleanenv"
 )
 
 var (
@@ -28,33 +29,17 @@ type Config struct {
 
 func newConfig() *Config {
 	var (
-		baseAddress     string
-		shorted         string
-		fileStoragePath string
-		exists          bool
-		dsnDB           string
+		cfg Config
 	)
 
-	if baseAddress, exists = os.LookupEnv("SERVER_ADDRESS"); !exists {
-		baseAddress = "localhost:8080"
-	}
-
-	if shorted, exists = os.LookupEnv("BASE_URL"); !exists {
-		shorted = "http://localhost:8080"
-	}
-
-	if fileStoragePath, exists = os.LookupEnv("FILE_STORAGE_PATH"); !exists {
-		fileStoragePath = "/tmp/shortener.json"
-	}
-
-	if dsnDB, exists = os.LookupEnv("DATABASE_DSN"); !exists {
-		dsnDB = "postgres://postgres:postgres@localhost:5432/postgres?sslmode=disable"
+	if err := cleanenv.ReadEnv(&cfg); err != nil {
+		panic(err)
 	}
 
 	return &Config{
-		Address:    baseAddress,
-		ShortedURL: shorted,
-		FileDB:     fileStoragePath,
-		DSN:        dsnDB,
+		Address:    cfg.Address,
+		ShortedURL: cfg.ShortedURL,
+		FileDB:     cfg.FileDB,
+		DSN:        cfg.DSN,
 	}
 }
