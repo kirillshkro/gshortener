cb54b4c6c40b79ffa111370a18d67a9168eb9f18
Kirill Shkrogalev <kirill.shkrogalev@gmail.com>
2026-03-13 23:34:21 +0300

    Added soft migration to Storage

--- internal/repository/storage/db_storage.go
+++ internal/repository/storage/db_storage.go
@@ -2,13 +2,21 @@ package storage
 
 import (
 	"database/sql"
+	"sync"
 
 	"github.com/kirillshkro/gshortener/internal/types"
+	_ "github.com/lib/pq"
+	goose "github.com/pressly/goose/v3"
 )
 
 type DBStorage struct {
 	db *sql.DB
 }
+
+var (
+	dbinstance *DBStorage
+	dbonce     sync.Once
+)
 
 func (s *DBStorage) Data(key types.ShortURL) (types.RawURL, error) {
 	return "", nil
@@ -18,15 +26,22 @@ func (s *DBStorage) SetData(urlData types.URLData) error {
 	return nil
 }
 
-func NewDBStorage(conn string) (*DBStorage, error) {
+func newDBStorage(conn string) (*DBStorage, error) {
 	db, err := sql.Open("postgres", conn)
 	if err != nil {
 		return nil, err
 	}
-	if err = db.Ping(); err != nil {
-		return nil, err
-	}
+
 	return &DBStorage{
 		db: db,
 	}, nil
+}
+
+func GetDBStorage(conn string) (*DBStorage, error) {
+	var err error
+	dbonce.Do(func() {
+		dbinstance, err = newDBStorage(conn)
+		err = goose.Up(dbinstance.db, "./migrations")
+	})
+	return dbinstance, err
 }
