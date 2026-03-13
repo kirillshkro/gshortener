6bc46942e22808723735967629d670d9d46e6e20
Kirill Shkrogalev <kirill.shkrogalev@gmail.com>
2026-03-13 19:09:43 +0300

    Add table migration

---
+++ migrations/20260313160729_add_table_urls.sql
@@ -0,0 +1,6 @@
+-- +goose Up
+SELECT 'up SQL query';
+create table urls (id serial primary key, short_url text not null, original_url text not null);
+-- +goose Down
+SELECT 'down SQL query';
+drop table urls;
