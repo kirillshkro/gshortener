33ce9162dde0113f802f5206100b5c3fba5d7f9a
Kirill Shkrogalev <kirill.shkrogalev@gmail.com>
2026-03-14 15:35:51 +0300

    Implemented Data in DBStorage

--- internal/repository/storage/db_storage.go
+++ internal/repository/storage/db_storage.go
@@ -19,7 +19,16 @@ var (
 )
 
 func (s *DBStorage) Data(key types.ShortURL) (types.RawURL, error) {
-	return "", nil
+	var originalUrl types.RawURL
+	if key != "" {
+		if row := s.db.QueryRowContext(context.Background(), "select original_url from urls where short_url = $1", key); row != nil {
+			if err := row.Scan(&originalUrl); err != nil {
+				return "", err
+			}
+			return originalUrl, nil
+		}
+	}
+	return "", types.ErrEmptyParams
 }
 
 func (s *DBStorage) SetData(urlData types.URLData) error {
