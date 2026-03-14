1454858a0e1993ac2b977ff182cad517c3b0d11c
Kirill Shkrogalev <kirill.shkrogalev@gmail.com>
2026-03-14 17:40:38 +0300

    Fixed migrations

--- migrations/20260313160729_add_table_urls.sql
+++ migrations/20260313160729_add_table_urls.sql
@@ -1,6 +1,6 @@
 -- +goose Up
 SELECT 'up SQL query';
-create table urls (id serial primary key, short_url text not null, original_url text not null);
+create table if not exists urls (id serial primary key, short_url text not null, original_url text not null);
 -- +goose Down
 SELECT 'down SQL query';
 drop table if exists urls;
