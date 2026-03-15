4cb62552bb0532368b44471ff0bb2730d334422a
Kirill Shkrogalev <kirill.shkrogalev@gmail.com>
2026-03-15 20:25:41 +0300

    Refactored test suite

--- internal/handler/shortener/service_test.go
+++ internal/handler/shortener/service_test.go
@@ -1,7 +1,9 @@
 package shortener
 
 import (
+	"bytes"
 	"encoding/json"
+	"io"
 	"net/http"
 	"net/http/httptest"
 	"strings"
@@ -10,9 +12,30 @@ import (
 	"github.com/gorilla/mux"
 	"github.com/kirillshkro/gshortener/internal/types"
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/suite"
 )
 
-func Test_URLEncode(t *testing.T) {
+type ServiceTestsSuite struct {
+	suite.Suite
+	service *Service
+	router  *mux.Router
+	server  *httptest.Server
+}
+
+func (s *ServiceTestsSuite) SetupSuite() {
+	s.router = mux.NewRouter()
+	s.server = httptest.NewServer(s.router)
+	s.service = NewServiceWithAddrWithAddrShortener(types.RawURL(s.server.URL), types.ShortURL(s.server.URL))
+	s.router.HandleFunc("/", s.service.URLEncode).Methods(http.MethodGet, http.MethodPost, http.MethodPut, http.MethodPatch)
+	s.router.HandleFunc("/{id}", s.service.URLDecode).Methods(http.MethodGet, http.MethodPost, http.MethodPut, http.MethodPatch)
+}
+
+func (s *ServiceTestsSuite) TearDownSuite() {
+	s.server.Close()
+	s.service.Stor.Close()
+}
+
+func (s *ServiceTestsSuite) Test_URLEncode() {
 	const testData = "https://practicum.yandex.ru"
 	tests := []struct {
 		name   string
@@ -51,17 +74,16 @@ func Test_URLEncode(t *testing.T) {
 		},
 	}
 	for _, test := range tests {
-		t.Run(test.name, func(t *testing.T) {
-			_, fakeServ := setup()
+		s.T().Run(test.name, func(t *testing.T) {
 			body, err := json.Marshal(test.uri)
 			if err != nil {
 				t.Fatal(err)
 			}
-			req, err := http.NewRequest(test.method, fakeServ.URL+"/", strings.NewReader(string(body)))
+			req, err := http.NewRequest(test.method, s.server.URL+"/", strings.NewReader(string(body)))
 			if err != nil {
 				t.Fatal(err)
 			}
-			client := fakeServ.Client()
+			client := s.server.Client()
 			resp, err := client.Do(req)
 			if err != nil {
 				t.Fatal(err)
@@ -74,15 +96,14 @@ func Test_URLEncode(t *testing.T) {
 	}
 }
 
-func Test_URLDecode(t *testing.T) {
+func (s *ServiceTestsSuite) Test_URLDecode() {
 	const testURL = `https://practicum.yandex.ru`
 	id := Hashing([]byte(testURL))
-	service, fakeServ := setup()
-	if err := service.Stor.SetData(types.URLData{
+	if err := s.service.Stor.SetData(types.URLData{
 		ShortURL:    types.ShortURL(id),
 		OriginalURL: types.RawURL(testURL),
 	}); err != nil {
-		t.Fatal(err)
+		s.T().Fatal(err)
 	}
 	tests := []struct {
 		name   string
@@ -117,14 +138,13 @@ func Test_URLDecode(t *testing.T) {
 	}
 
 	for _, test := range tests {
-		t.Run(test.name, func(t *testing.T) {
-			defer fakeServ.Close()
-			req, err := http.NewRequest(test.method, fakeServ.URL+"/"+string(id), nil)
+		s.T().Run(test.name, func(t *testing.T) {
+			req, err := http.NewRequest(test.method, s.server.URL+"/"+string(id), nil)
 			if err != nil {
 				t.Fatal(err)
 			}
 			rr := httptest.NewRecorder()
-			service.URLDecode(rr, req)
+			s.service.URLDecode(rr, req)
 			resp := rr.Result()
 			defer resp.Body.Close()
 			assert.Equal(t, test.status, resp.StatusCode)
@@ -136,11 +156,66 @@ func Test_URLDecode(t *testing.T) {
 	}
 }
 
-func setup() (*Service, *httptest.Server) {
-	router := mux.NewRouter()
-	fakeServ := httptest.NewServer(router)
-	service := NewServiceWithAddrWithAddrShortener(types.RawURL(fakeServ.URL), types.ShortURL(fakeServ.URL))
-	router.HandleFunc("/", service.URLEncode).Methods(http.MethodGet, http.MethodPost, http.MethodPut, http.MethodPatch)
-	router.HandleFunc("/{id}", service.URLDecode).Methods(http.MethodGet, http.MethodPost, http.MethodPut, http.MethodPatch)
-	return service, fakeServ
+func (s *ServiceTestsSuite) Test_CreateShortURL() {
+	var (
+		mockResp bytes.Buffer
+		mockReq  types.RequestData
+	)
+	testURL := types.RawURL("https://weather.yandex.ru")
+	mockReq.URL = testURL
+	testBody, err := json.Marshal(mockReq)
+	if err != nil {
+		s.T().Fatal(err)
+	}
+	id := Hashing([]byte(testURL))
+	shortedURL := types.ShortURL(s.server.URL) + "/" + id
+	respData := types.ResponseData{
+		Result: shortedURL,
+	}
+	if err := json.NewEncoder(&mockResp).Encode(respData); err != nil {
+		s.T().Fatal(err)
+	}
+	tests := []struct {
+		name   string
+		method string
+		body   []byte
+		status int
+	}{
+		{
+			name:   "Normal POST request",
+			method: http.MethodPost,
+			body:   testBody,
+			status: http.StatusCreated,
+		},
+		{
+			name:   "Empty body POST request",
+			method: http.MethodPost,
+			body:   []byte(""),
+			status: http.StatusBadRequest,
+		},
+		{
+			name:   "Wrong GET request",
+			method: http.MethodGet,
+			body:   []byte(""),
+			status: http.StatusBadRequest,
+		},
+	}
+	for _, test := range tests {
+		s.T().Run(test.name, func(t *testing.T) {
+			rr := httptest.NewRecorder()
+			req := httptest.NewRequest(test.method, s.server.URL+"/api/shortlen", bytes.NewBuffer(test.body))
+			s.service.CreateShortURL(rr, req)
+			resp := rr.Result()
+			defer resp.Body.Close()
+			assert.Equalf(t, test.status, resp.StatusCode, "Test failed by status code expected: %d, actual: %d\n", test.status, resp.StatusCode)
+			if resp.StatusCode == http.StatusCreated {
+				rBody, _ := io.ReadAll(resp.Body)
+				assert.JSONEqf(t, string(rBody), mockResp.String(), "Test failed by body expected: %v, actual: %v\n", mockResp.String(), string(rBody))
+			}
+		})
+	}
+}
+
+func Test_Main(t *testing.T) {
+	suite.Run(t, new(ServiceTestsSuite))
 }
