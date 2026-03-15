7db83e48a2eb4107806dbcc71b845be9c795db77
Kirill Shkrogalev <kirill.shkrogalev@gmail.com>
2026-03-15 17:15:45 +0300

    Added handler

--- internal/handler/shortener/middleware/middleware.go
+++ internal/handler/shortener/middleware/middleware.go
@@ -63,3 +63,7 @@ func CreateShortURLHandler(s shortener.JSONEncoder) http.Handler {
 func PingHandler(s shortener.Pinger) http.Handler {
 	return http.HandlerFunc(s.Ping)
 }
+
+func BatchCreateUrlHandler(s shortener.IService) http.Handler {
+	return http.HandlerFunc(s.BatchCreateShortURL)
+}
