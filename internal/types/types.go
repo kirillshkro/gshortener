aecca5c30d1e53016dc51f31fbea2a3cb8b4a8f6
Kirill Shkrogalev <kirill.shkrogalev@gmail.com>
2026-03-15 10:35:29 +0300

    Added new types

--- internal/types/types.go
+++ internal/types/types.go
@@ -22,3 +22,13 @@ type RequestData struct {
 type ResponseData struct {
 	Result string `json:"result"`
 }
+
+type BatchRequest struct {
+	CorrelationID string `json:"correlation_id"`
+	OriginalURL   string `json:"original_url"`
+}
+
+type BatchResponse struct {
+	CorrelationID string `json:"correlation_id"`
+	ShortURL      string `json:"short_url"`
+}
