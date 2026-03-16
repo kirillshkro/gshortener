0c967d5b96da8bd35dc54834faaf51aea30edc33
Kirill Shkrogalev <kirill.shkrogalev@gmail.com>
2026-03-16 17:54:30 +0300

    Added custom error

--- internal/types/app_errors.go
+++ internal/types/app_errors.go
@@ -6,3 +6,11 @@ var (
 	ErrEmptyParams = errors.New("empty params")
 	ErrFileOpen    = errors.New("file not opened")
 )
+
+type ErrDuplicateKey struct {
+	key string
+}
+
+func (e *ErrDuplicateKey) Error() string {
+	return "key " + e.key + " already exists"
+}
