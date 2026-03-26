229713a664364a2217ce11dce53ddb49930eda1d
Kirill Shkrogalev <kirill.shkrogalev@gmail.com>
2026-03-26 09:31:43 +0300

    Updated types

--- internal/types/types.go
+++ internal/types/types.go
@@ -6,10 +6,11 @@ type RawURL string
 type ShortURL string
 
 type DataURL struct {
-	ID          uint        `gorm:"not null;primaryKey"`
-	ShortURL    ShortURL    `json:"short_url" gorm:"type:varchar(10);not null;uniqueIndex:idx_short_url"`
-	OriginalURL RawURL      `json:"original_url" gorm:"not null;uniqueIndex:idx_original_url"`
-	UserProfile UserProfile `json:"user_profile" gorm:"foreignKey"`
+	ID            uint        `gorm:"not null;primaryKey"`
+	ShortURL      ShortURL    `json:"short_url" gorm:"type:varchar(10);not null;uniqueIndex:idx_short_url"`
+	OriginalURL   RawURL      `json:"original_url" gorm:"not null;uniqueIndex:idx_original_url"`
+	UserProfileID string      `json:"user_profile_uuid" gorm:"type:varchar(36);not null;index:idx_user_profile"`
+	UserProfile   UserProfile `json:"user_profile" gorm:"foreignKey:UserProfileID;references:Id;constraint:OnDelete:CASCADE"`
 }
 
 type FileData struct {
