abc3a9e3a824c25ca142b19172f067e06a4ea476
Kirill Shkrogalev <kirill.shkrogalev@gmail.com>
2026-03-30 22:38:47 +0300

    Updated models

--- internal/config/config.go
+++ internal/config/config.go
@@ -1,7 +1,9 @@
 package config
 
 import (
+	"os"
 	"sync"
+	"time"
 
 	"github.com/ilyakaznacheev/cleanenv"
 )
@@ -25,6 +27,13 @@ type Config struct {
 	ShortedURL string `json:"shorted_url" env:"SHORTED_URL" env-default:"http://localhost:8080"`
 	FileDB     string `json:"file_db" env:"FILE_STORAGE_PATH" env-default:"/tmp/shortener.json"`
 	DSN        string `json:"dsn" env:"DATABASE_DSN"`
+	CookieConfig
+}
+
+type CookieConfig struct {
+	SecretKey string        `json:"secret_key" env:"AUTH_SECRET_KEY"`
+	Name      string        `json:"name" env:"COOKIE_NAME"`
+	MaxAge    time.Duration `json:"max_age" env:"COOKIE_MAX_AGE"`
 }
 
 func newConfig() *Config {
@@ -41,5 +50,10 @@ func newConfig() *Config {
 		ShortedURL: cfg.ShortedURL,
 		FileDB:     cfg.FileDB,
 		DSN:        cfg.DSN,
+		CookieConfig: CookieConfig{
+			SecretKey: os.Getenv("AUTH_SECRET_KEY"),
+			Name:      "auth_token",
+			MaxAge:    3600 * time.Second,
+		},
 	}
 }
--- internal/types/model/urls.go
+++ internal/types/model/urls.go
@@ -4,8 +4,8 @@ import "github.com/kirillshkro/gshortener/internal/types"
 
 type DataURL struct {
 	ID            uint           `gorm:"not null;primaryKey"`
-	ShortURL      types.ShortURL `json:"short_url" gorm:"type:varchar(10);not null;uniqueIndex:idx_short_url"`
-	OriginalURL   types.RawURL   `json:"original_url" gorm:"not null;uniqueIndex:idx_original_url"`
-	UserProfileID string         `json:"user_profile_uuid" gorm:"type:varchar(36);not null;index:idx_user_profile"`
+	ShortURL      types.ShortURL `json:"short_url" gorm:"type:varchar(10);not null;uniqueIndex"`
+	OriginalURL   types.RawURL   `json:"original_url" gorm:"not null;uniqueIndex"`
+	UserProfileID int            `json:"user_profile_uuid" gorm:"not null;index"`
 	UserProfile   UserProfile    `json:"user_profile" gorm:"foreignKey:UserProfileID;references:Id;constraint:OnDelete:CASCADE"`
 }
