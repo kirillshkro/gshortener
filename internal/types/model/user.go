561e3853c1f8fb66e88860529397c448c72b18b4
Kirill Shkrogalev <kirill.shkrogalev@gmail.com>
2026-03-30 11:26:39 +0300

    Changed UserProfile model

--- internal/types/model/user.go
+++ internal/types/model/user.go
@@ -1,8 +1,15 @@
 package model
 
+import (
+	"net/http"
+	"time"
+)
+
 // Профиль юзера: ID и список его ссылок, признак авторизации
 type UserProfile struct {
-	Id         int       `json:"uuid" gorm:"primaryKey"`
-	Authorized bool      `json:"authorized" gorm:"not null"`
-	URLs       []DataURL `json:"urls" gorm:"foreignKey:UserProfileID"`
+	Id         int          `json:"uuid" gorm:"primaryKey"`
+	Authorized bool         `json:"authorized" gorm:"not null"`
+	CreatedAt  time.Time    `json:"created_at" gorm:"not null"`
+	Cookie     *http.Cookie `json:"cookie"`
+	URLs       []DataURL    `json:"urls" gorm:"foreignKey:UserProfileID"`
 }
