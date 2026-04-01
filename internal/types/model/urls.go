b9cdce2ee4870c2c463f84e5fc7b4ee13e6720ba
Kirill Shkrogalev <kirill.shkrogalev@gmail.com>
2026-04-01 19:23:39 +0300

    Next fixes

--- internal/types/model/urls.go
+++ internal/types/model/urls.go
@@ -3,9 +3,9 @@ package model
 import "github.com/kirillshkro/gshortener/internal/types"
 
 type DataURL struct {
-	ID            uint           `gorm:"not null;primaryKey"`
+	ID            int            `gorm:"primaryKey"`
 	ShortURL      types.ShortURL `json:"short_url" gorm:"type:varchar(10);not null;uniqueIndex"`
 	OriginalURL   types.RawURL   `json:"original_url" gorm:"not null;uniqueIndex"`
 	UserProfileID int            `json:"user_profile_uuid" gorm:"not null;index"`
-	UserProfile   UserProfile    `gorm:"foreignKey:UserProfileID;references:ID;constraint:OnDelete:CASCADE"`
+	UserProfile   UserProfile    `gorm:"foreignKey:UserProfileID;references:ID"`
 }
