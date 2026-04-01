69582de434e3aa99d12283dac14adfcf33f587c7
Kirill Shkrogalev <kirill.shkrogalev@gmail.com>
2026-04-01 19:34:33 +0300

    Change model

--- internal/types/model/urls.go
+++ internal/types/model/urls.go
@@ -7,5 +7,5 @@ type DataURL struct {
 	ShortURL      types.ShortURL `json:"short_url" gorm:"type:varchar(10);not null;uniqueIndex"`
 	OriginalURL   types.RawURL   `json:"original_url" gorm:"not null;uniqueIndex"`
 	UserProfileID int            `json:"user_profile_uuid" gorm:"not null;index"`
-	UserProfile   UserProfile    `gorm:"foreignKey:UserProfileID;references:ID"`
+	UserProfile   UserProfile
 }
