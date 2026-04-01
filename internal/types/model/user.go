8b9704b87e5e21b0f0699e91e3e28e6b2d45aaa9
Kirill Shkrogalev <kirill.shkrogalev@gmail.com>
2026-04-01 19:47:21 +0300

    Foreign keys

--- internal/types/model/user.go
+++ internal/types/model/user.go
@@ -6,8 +6,8 @@ import (
 
 // Профиль юзера: ID и список его ссылок, признак авторизации
 type UserProfile struct {
-	ID         int       `json:"uuid" gorm:"primaryKey"`
+	ID         int       `json:"id" gorm:"primaryKey"`
 	Authorized bool      `json:"authorized" gorm:"not null"`
 	CreatedAt  time.Time `json:"created_at" gorm:"not null"`
-	URLs       []DataURL `json:"urls" gorm:"foreignKey:UserProfileID"`
+	URLs       []DataURL `json:"urls" gorm:"foreignKey:UserProfileID;references:ID"`
 }
