e9938e54637dcd5ae01a9abd96e3f10c9bc99dd3
Kirill Shkrogalev <kirill.shkrogalev@gmail.com>
2026-04-01 18:27:16 +0300

    Changed models

--- internal/types/model/user.go
+++ internal/types/model/user.go
@@ -9,5 +9,5 @@ type UserProfile struct {
 	ID         int       `json:"uuid" gorm:"primaryKey"`
 	Authorized bool      `json:"authorized" gorm:"not null"`
 	CreatedAt  time.Time `json:"created_at" gorm:"not null"`
-	URLs       []DataURL `json:"urls" gorm:"foreignKey"`
+	URLs       []DataURL `json:"urls" gorm:"foreignKey:UserProfileID"`
 }
