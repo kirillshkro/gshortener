fd5529630cf4163b70141ba1a186e60d538fe5d6
Kirill Shkrogalev <kirill.shkrogalev@gmail.com>
2026-04-01 19:40:02 +0300

    Checked

--- internal/types/model/urls.go
+++ internal/types/model/urls.go
@@ -6,6 +6,6 @@ type DataURL struct {
 	ID            int            `gorm:"primaryKey"`
 	ShortURL      types.ShortURL `json:"short_url" gorm:"type:varchar(10);not null;uniqueIndex"`
 	OriginalURL   types.RawURL   `json:"original_url" gorm:"not null;uniqueIndex"`
-	UserProfileID int            `json:"user_profile_uuid" gorm:"not null;index"`
+	UserProfileID int            `json:"user_profile_id" gorm:"not null;index"`
 	UserProfile   UserProfile
 }
