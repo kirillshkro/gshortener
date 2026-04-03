ee012040c8f19832b588c0c696de66515c548513
Kirill Shkrogalev <kirill.shkrogalev@gmail.com>
2026-04-03 17:36:51 +0300

    Changed model

--- internal/types/model/urls.go
+++ internal/types/model/urls.go
@@ -1,10 +1,12 @@
 package model
 
-import "github.com/kirillshkro/gshortener/internal/types"
+import (
+	"github.com/kirillshkro/gshortener/internal/types"
+)
 
 type DataURL struct {
 	ID            int            `gorm:"primaryKey"`
 	ShortURL      types.ShortURL `json:"short_url" gorm:"type:varchar(10);not null;uniqueIndex"`
 	OriginalURL   types.RawURL   `json:"original_url" gorm:"not null;uniqueIndex"`
-	UserProfileID int            `json:"user_profile_id" gorm:"not null;index"`
+	UserProfileID string         `json:"user_profile_id" gorm:"not null;index"` //UUID
 }
