7bdcf950bd60f963a983bbbce0d663f3d6cf671e
Kirill Shkrogalev <kirill.shkrogalev@gmail.com>
2026-04-07 14:17:43 +0300

    Added json tags

--- internal/types/types.go
+++ internal/types/types.go
@@ -24,6 +24,6 @@ type BatchResponse struct {
 }
 
 type UserURL struct {
-	ShortURL    ShortURL
-	OriginalURL RawURL
+	ShortURL    string `json:"short_url"`
+	OriginalURL string `json:"original_url"`
 }
