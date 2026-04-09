505b53c055fa4ae74b039c7677cd4dfe6705ad9e
Kirill Shkrogalev <kirill.shkrogalev@gmail.com>
2026-04-09 23:40:21 +0300

    Refresh cookie

--- internal/handler/shortener/user_urls.go
+++ internal/handler/shortener/user_urls.go
@@ -18,64 +18,67 @@ func (s Service) GetUserURLs(resp http.ResponseWriter, req *http.Request) {
 		resp.WriteHeader(http.StatusMethodNotAllowed)
 		return
 	}
-	cookie, err := req.Cookie("auth_cookie")
-	if err != nil {
-		resp.WriteHeader(http.StatusNoContent)
-		//обновим cookie
-		authCfg := auth.NewAuthConfig()
-		authUser := claims.NewAuthUser(authCfg)
-		token, err := authUser.Token()
+	if cookieExist(req, "auth_cookie") {
+		cookie, err := req.Cookie("auth_cookie")
 		if err != nil {
-			resp.WriteHeader(http.StatusInternalServerError)
+			resp.WriteHeader(http.StatusNoContent)
+		}
+		//Получаем токен из cookie
+		//и проверяем его на валидность
+		token := cookie.Value
+		if token == "" {
+			resp.WriteHeader(http.StatusUnauthorized)
 			return
 		}
-		updatedCookie := &http.Cookie{
-			Name:     "auth_cookie",
-			Value:    token,
-			MaxAge:   3600 * 24 * 7, // 7 дней
-			Path:     "/",
-			Secure:   false,
-			HttpOnly: true,
+
+		userID, err := claims.GetUserID(token)
+		if err != nil {
+			resp.WriteHeader(http.StatusNoContent)
+			return
 		}
-		http.SetCookie(resp, updatedCookie)
-		return
-	}
-	//Получаем токен из cookie
-	//и проверяем его на валидность
-	token := cookie.Value
-	if token == "" {
-		resp.WriteHeader(http.StatusUnauthorized)
-		return
-	}
+		//Получаем все URL пользователя по его ID
+		urls, err := s.Stor.GetUserURLs(userID)
+		if err != nil {
+			resp.WriteHeader(http.StatusNoContent)
+			return
+		}
+		if len(urls) == 0 {
+			resp.WriteHeader(http.StatusNoContent)
+			return
+		}
 
-	userID, err := claims.GetUserID(token)
-	if err != nil {
-		resp.WriteHeader(http.StatusNoContent)
-		return
-	}
-	//Получаем все URL пользователя по его ID
-	urls, err := s.Stor.GetUserURLs(userID)
-	if err != nil {
-		resp.WriteHeader(http.StatusNoContent)
-		return
-	}
-	if len(urls) == 0 {
-		resp.WriteHeader(http.StatusNoContent)
-		return
-	}
-
-	//Отдаем пользователю все URL
-	resp.Header().Set("Content-Type", "application/json")
-	resp.WriteHeader(http.StatusOK)
-	var userURLs []types.UserURL
-	for _, url := range urls {
-		userURLs = append(userURLs, types.UserURL{
-			ShortURL:    string(s.ResultAddr) + "/" + url.ShortURL,
-			OriginalURL: url.OriginalURL,
-		})
-	}
-	if err = json.NewEncoder(resp).Encode(userURLs); err != nil {
-		s.logger.Error("cannot encode response: ", "error: ", err.Error())
-		return
+		//Отдаем пользователю все URL
+		resp.Header().Set("Content-Type", "application/json")
+		resp.WriteHeader(http.StatusOK)
+		var userURLs []types.UserURL
+		for _, url := range urls {
+			userURLs = append(userURLs, types.UserURL{
+				ShortURL:    string(s.ResultAddr) + "/" + url.ShortURL,
+				OriginalURL: url.OriginalURL,
+			})
+		}
+		if err = json.NewEncoder(resp).Encode(userURLs); err != nil {
+			s.logger.Error("cannot encode response: ", "error: ", err.Error())
+			return
+		} else {
+			//обновим cookie
+			authCfg := auth.NewAuthConfig()
+			authUser := claims.NewAuthUser(authCfg)
+			token, err := authUser.Token()
+			if err != nil {
+				resp.WriteHeader(http.StatusInternalServerError)
+				return
+			}
+			updatedCookie := &http.Cookie{
+				Name:     "auth_cookie",
+				Value:    token,
+				MaxAge:   3600 * 24 * 7, // 7 дней
+				Path:     "/",
+				Secure:   false,
+				HttpOnly: true,
+			}
+			http.SetCookie(resp, updatedCookie)
+			return
+		}
 	}
 }
