17dcfa217b5a34c8a407769f6fb905bac010c63d
Kirill Shkrogalev <kirill.shkrogalev@gmail.com>
2026-04-14 19:19:42 +0300

    Added send code error

--- internal/handler/shortener/delete_urls.go
+++ internal/handler/shortener/delete_urls.go
@@ -15,31 +15,37 @@ type Deleter interface {
 
 func (s Service) DeleteUserURLs(resp http.ResponseWriter, req *http.Request) {
 
-	if cookieExist(req, "auth_cookie") {
-		authUser := claims.NewAuthUser(auth.NewAuthConfig())
-		userToken, err := authUser.Token()
-		if err != nil {
-			s.logger.Error("failed to get user token", "error", err)
-			return
-		}
+	if !cookieExist(req, "auth_cookie") {
+		s.logger.Error("auth cookie not found")
+		resp.WriteHeader(http.StatusUnauthorized)
+		return
+	}
 
-		userID, err := claims.GetUserID(userToken)
-		if err != nil {
-			s.logger.Error("failed to get user ID", "error", err)
-			return
-		}
+	authUser := claims.NewAuthUser(auth.NewAuthConfig())
+	userToken, err := authUser.Token()
+	if err != nil {
+		s.logger.Error("failed to get user token", "error", err)
+		resp.WriteHeader(http.StatusInternalServerError)
+		return
+	}
 
-		var urls []types.ShortURL
+	userID, err := claims.GetUserID(userToken)
+	if err != nil {
+		s.logger.Error("failed to get user ID", "error", err)
+		resp.WriteHeader(http.StatusInternalServerError)
+		return
+	}
 
-		if err := json.NewDecoder(req.Body).Decode(&urls); err != nil {
-			s.logger.Error("failed to decode request body", "error", err)
-			return
-		}
+	var urls []types.ShortURL
 
-		for _, url := range urls {
-			go s.Stor.DeleteUserURL(userID, url)
-			s.logger.Info("deleted user URL", "url", url)
-		}
+	if err := json.NewDecoder(req.Body).Decode(&urls); err != nil {
+		s.logger.Error("failed to decode request body", "error", err)
+		return
+	}
+
+	for _, url := range urls {
+		go s.Stor.DeleteUserURL(userID, url)
+		s.logger.Info("deleted user URL", "url", url)
 	}
 
 	resp.WriteHeader(http.StatusAccepted)
