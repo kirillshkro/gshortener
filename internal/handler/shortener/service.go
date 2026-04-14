9244903586ca8d72d0ef1ace68f0bb4bb26678a9
Kirill Shkrogalev <kirill.shkrogalev@gmail.com>
2026-04-14 13:37:51 +0300

    Rollback in service

--- internal/handler/shortener/service.go
+++ internal/handler/shortener/service.go
@@ -13,6 +13,7 @@ import (
 	"strings"
 
 	"github.com/kirillshkro/gshortener/internal/config"
+	"github.com/kirillshkro/gshortener/internal/config/auth"
 	"github.com/kirillshkro/gshortener/internal/handler/shortener/claims"
 	"github.com/kirillshkro/gshortener/internal/repository/storage"
 	"github.com/kirillshkro/gshortener/internal/types"
@@ -110,6 +111,7 @@ func (s Service) URLEncode(resp http.ResponseWriter, req *http.Request) {
 	var (
 		token    string
 		userUUID string
+		cookie   *http.Cookie
 	)
 	if cookieExist(req, "auth_cookie") {
 		cookie, err := req.Cookie("auth_cookie")
@@ -127,7 +129,22 @@ func (s Service) URLEncode(resp http.ResponseWriter, req *http.Request) {
 			return
 		}
 	} else {
-		token = s.refreshUserCookie(resp)
+		authCfg := auth.NewAuthConfig()
+		authUser := claims.NewAuthUser(authCfg)
+		token, err = authUser.Token()
+		if err != nil {
+			http.Error(resp, "can't get token", http.StatusInternalServerError)
+			return
+		}
+		cookie = &http.Cookie{
+			Name:     "auth_cookie",
+			Value:    token,
+			MaxAge:   3600 * 24 * 7, // 7 дней
+			Path:     "/",
+			Secure:   false,
+			HttpOnly: true,
+		}
+		http.SetCookie(resp, cookie)
 		if userUUID, err = claims.GetUserID(token); err != nil {
 			http.Error(resp, "can't get user id", http.StatusInternalServerError)
 			return
