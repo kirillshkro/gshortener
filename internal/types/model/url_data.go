3e50c28243daf078b2e4ac77af2669bd79b02e70
Kirill Shkrogalev <kirill.shkrogalev@gmail.com>
2026-04-14 18:22:10 +0300

    Changed model

--- internal/types/model/url_data.go
+++ internal/types/model/url_data.go
@@ -7,4 +7,5 @@ type URLData struct {
 	ShortURL    types.ShortURL `json:"short_url" gorm:"not null;uniqueIndex"`
 	OriginalURL types.RawURL   `json:"original_url" gorm:"not null;uniqueIndex"`
 	UserUUID    string         `json:"user_uuid" gorm:"not null;index"`
+	IsDeleted   bool           `gorm:"not null;default:false"`
 }
