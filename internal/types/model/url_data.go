f3a75746828eb86f6042c5c8674646627e399186
Kirill Shkrogalev <kirill.shkrogalev@gmail.com>
2026-04-14 11:13:22 +0300

    Added flag deletion field

--- internal/types/model/url_data.go
+++ internal/types/model/url_data.go
@@ -6,5 +6,6 @@ type URLData struct {
 	ID          uint           `gorm:"not null;primaryKey"`
 	ShortURL    types.ShortURL `json:"short_url" gorm:"not null;uniqueIndex"`
 	OriginalURL types.RawURL   `json:"original_url" gorm:"not null;uniqueIndex"`
-	UserUUID    string         `json:"user_uuid" gorm:"not null;index"`
+	UserID      string         `json:"user_uuid" gorm:"not null;index"`
+	IsDeleted   bool           `gorm:"default:false"`
 }
