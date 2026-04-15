871f9d0fc341b9e4e1d1e083ee8d417169e8a277
Kirill Shkrogalev <kirill.shkrogalev@gmail.com>
2026-04-15 19:27:51 +0300

    Added synchronization

--- internal/handler/shortener/delete_urls.go
+++ internal/handler/shortener/delete_urls.go
@@ -3,6 +3,7 @@ package shortener
 import (
 	"encoding/json"
 	"net/http"
+	"sync"
 
 	"github.com/kirillshkro/gshortener/internal/config/auth"
 	"github.com/kirillshkro/gshortener/internal/handler/shortener/claims"
@@ -36,16 +37,23 @@ func (s Service) DeleteUserURLs(resp http.ResponseWriter, req *http.Request) {
 		return
 	}
 
-	var urls []types.ShortURL
+	var (
+		urls []types.ShortURL
+		wg   sync.WaitGroup
+	)
 
 	if err := json.NewDecoder(req.Body).Decode(&urls); err != nil {
 		s.logger.Error("failed to decode request body", "error", err)
 		return
 	}
-
+	wg.Add(len(urls))
 	for _, url := range urls {
-		go s.Stor.DeleteUserURL(userID, url)
-		s.logger.Info("deleted user URL", "url", url)
+		go func(url types.ShortURL) {
+			defer wg.Done()
+			if err := s.Stor.DeleteUserURL(userID, url); err != nil {
+				s.logger.Error("failed to delete URL", "error", err)
+			}
+		}(url)
 	}
 
 	resp.WriteHeader(http.StatusAccepted)
