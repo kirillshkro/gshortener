6cd916b93669694589e9a7143866b78c42f736fe
Kirill Shkrogalev <kirill.shkrogalev@gmail.com>
2026-04-17 21:22:28 +0300

    Added errors processing

--- internal/handler/shortener/delete_urls.go
+++ internal/handler/shortener/delete_urls.go
@@ -3,6 +3,7 @@ package shortener
 import (
 	"encoding/json"
 	"net/http"
+	"time"
 
 	"github.com/kirillshkro/gshortener/internal/handler/shortener/claims"
 	"github.com/kirillshkro/gshortener/internal/types"
@@ -49,8 +50,29 @@ func (s Service) DeleteUserURLs(resp http.ResponseWriter, req *http.Request) {
 		return
 	}
 
+	errChan := make(chan error, len(urls))
+
 	for _, url := range urls {
-		go s.Stor.DeleteUserURL(userID, url)
+		go func(url types.ShortURL) {
+			errChan <- s.Stor.DeleteUserURL(userID, url)
+		}(url)
 	}
+
+	go func() {
+		timeout := time.After(10 * time.Second)
+		errCount := 0
+		for i := 0; i < len(urls); i++ {
+			select {
+			case err := <-errChan:
+				if err != nil {
+					s.logger.Error("failed to delete url", "url", string(urls[i]), "error", err)
+					errCount++
+				}
+			case <-timeout:
+				s.logger.Error("delete urls timeout")
+				return
+			}
+		}
+	}()
 	resp.WriteHeader(http.StatusAccepted)
 }
