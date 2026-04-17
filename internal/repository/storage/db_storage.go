705e2f5b7cbaf065a0ae924d1761384fd7ca80f4
Kirill Shkrogalev <kirill.shkrogalev@gmail.com>
2026-04-17 11:59:58 +0300

    Deleted unassary comparision

--- internal/repository/storage/db_storage.go
+++ internal/repository/storage/db_storage.go
@@ -147,7 +147,7 @@ func (s *DBStorage) DeleteUserURL(userID string, shortURL types.ShortURL) error 
 	}
 	ctx := context.Background()
 	err := s.db.Transaction(func(tx *gorm.DB) error {
-		if _, err := gorm.G[model.URLData](tx).Where("user_uuid = ? AND short_url = ? AND is_deleted = false", userID, shortURL).
+		if _, err := gorm.G[model.URLData](tx).Where("user_uuid = ? AND short_url = ?", userID, shortURL).
 			Update(ctx, "is_deleted", true); err != nil {
 			tx.Rollback()
 			return err
