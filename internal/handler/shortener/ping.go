9d12858d02f2f2ae589bf654f85e4ee5e57e567e
Kirill Shkrogalev <kirill.shkrogalev@gmail.com>
2026-06-21 17:10:55 +0300

    Added documentation to interface Pinger

--- internal/handler/shortener/ping.go
+++ internal/handler/shortener/ping.go
@@ -11,10 +11,16 @@ import (
 	_ "github.com/lib/pq"
 )
 
+// Pinger interface defines the contract for handling ping requests.
 type Pinger interface {
-	Ping(w http.ResponseWriter, req *http.Request)
+	Ping(w http.ResponseWriter, r *http.Request)
 }
 
+// Ping handles the health check request to verify database connectivity.
+//
+// Parameters:
+//   - w: The HTTP response writer to send the response back to the client.
+//   - r: The HTTP request containing the health check information.
 func (s Service) Ping(w http.ResponseWriter, r *http.Request) {
 	var (
 		db  *sql.DB
