aa29d205e94f2d159697f5f458d3f942a472822c
Kirill Shkrogalev <kirill.shkrogalev@gmail.com>
2026-06-25 12:20:50 +0300

    Added godoc to file_data

--- internal/model/file_data.go
+++ internal/model/file_data.go
@@ -1,6 +1,10 @@
+// Package model contains data models for the application
 package model
 
+// FileData represents the structure for file data with UUID and URL information
 type FileData struct {
+	// UUID is the unique identifier for the file
 	UUID string `json:"uuid"`
+	// URLData contains the URL-related information
 	URLData
 }
