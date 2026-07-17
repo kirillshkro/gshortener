13f00868f3cb346cced7cdc65f5e34569526d314
Kirill Shkrogalev <kirill.shkrogalev@gmail.com>
2026-07-17 11:11:36 +0300

    Added benchmark to CreateShortURL

--- internal/handler/shortener/service_bench_test.go
+++ internal/handler/shortener/service_bench_test.go
@@ -1,10 +1,14 @@
 package shortener
 
 import (
+	"bytes"
+	"encoding/json"
 	"net/http"
 	"net/http/httptest"
+	"strings"
 	"testing"
 
+	"github.com/kirillshkro/gshortener/internal/types"
 	"github.com/kirillshkro/gshortener/pkg/urlgen"
 )
 
@@ -13,11 +17,14 @@ func Benchmark_URLEncode(b *testing.B) {
 	s.SetT(&testing.T{})
 	s.SetupSuite()
 	url := urlgen.GenerateURL("http://basedurl")
-	req := httptest.NewRequest(http.MethodPost, url, nil)
+	body, err := json.Marshal(url)
+	if err != nil {
+		b.Fatal(err)
+	}
+	req := httptest.NewRequest(http.MethodPost, s.server.URL+"/", strings.NewReader(string(body)))
 	rr := httptest.NewRecorder()
 	b.ResetTimer()
 	for b.Loop() {
-
 		s.service.URLEncode(rr, req)
 	}
 }
@@ -28,10 +35,31 @@ func Benchmark_URLDecode(b *testing.B) {
 	s.SetupSuite()
 	url := urlgen.GenerateURL("http://basedurl")
 	shortedUrl := Hashing([]byte(url))
-	req := httptest.NewRequest(http.MethodGet, "/"+string(shortedUrl), nil)
+	req := httptest.NewRequest(http.MethodGet, s.server.URL+"/"+string(shortedUrl), nil)
 	rr := httptest.NewRecorder()
 	b.ResetTimer()
 	for b.Loop() {
 		s.service.URLDecode(rr, req)
+	}
+}
+
+func Benchmark_CreateURL(b *testing.B) {
+	s := new(ServiceTestsSuite)
+	s.SetT(&testing.T{})
+	s.SetupSuite()
+	url := urlgen.GenerateURL("http://basedurl")
+	rr := httptest.NewRecorder()
+	b.ResetTimer()
+	for b.Loop() {
+		rBody := types.RequestData{
+			URL: types.RawURL(url),
+		}
+		reqURL, err := json.Marshal(rBody)
+		if err != nil {
+			s.T().Fatal(err)
+		}
+		req := httptest.NewRequest(http.MethodPost, s.server.URL+"/api/shorten", bytes.NewReader(reqURL))
+		req.Header.Set("Content-Type", "application/json")
+		s.service.CreateShortURL(rr, req)
 	}
 }
